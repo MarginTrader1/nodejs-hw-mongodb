80aaacce125a828070f1fb2105f70ad25d4d9794
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-09-22 20:50:07 +0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,11 @@
 import { UserCollection } from '../db/models/User.js';
 
-// монгус метод для регистрации нового юзера - возвращает внесенные данные 
+// монгус метод для регистрации нового юзера - возвращает данные в виде сложного объекта
 export const signup = async (payload) => {
   const user = await UserCollection.create(payload);
+
+  // удаляем пароль из ответа c помощью delete
+  delete user._doc.password;
+
   return user;
 };
