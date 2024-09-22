8281699dfbd539a158cc6a42e7cc29960ab0ee20
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-09-22 21:37:24 +0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,17 @@
 import { UserCollection } from '../db/models/User.js';
+import createHttpError from 'http-errors';
 
 // монгус метод для регистрации нового юзера - возвращает данные в виде сложного объекта
 export const signup = async (payload) => {
+  // проверка на наличие юзера в базе данных перед добавлением
+  const { email } = payload;
+  const checkUser = await UserCollection.findOne({ email });
+  // если юзер есть - выкидуем ошибку 409
+  if (checkUser) {
+    throw createHttpError(409, 'Email already exist');
+  }
+
+  // если нет - создаем новый документ в базе данных
   const user = await UserCollection.create(payload);
 
   // удаляем пароль из ответа c помощью delete
