bd0368ec7826a83894e7a7ca9258c2048001b5a6
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-09-23 00:10:57 +0300

    Update users.js

--- src/constants/users.js
+++ src/constants/users.js
@@ -1,6 +1,6 @@
 // шаблон для валидации email
 export const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-// час життя - милисекунды * секунды * хвилини
+// час життя - милисекунды * секунды * хвилини * години * дни
 export const accessTokenLifetime = 1000 * 60 * 15;
-export const refreshTokenLifetime = 1000 * 60 * 60 * 24;
+export const refreshTokenLifetime = 1000 * 60 * 60 * 24 * 30;
